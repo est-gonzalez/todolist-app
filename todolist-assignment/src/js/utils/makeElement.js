5ca13eda4fd785a9e368f2177109712bd5b3295b
est-gonzalez <77519111+est-gonzalez@users.noreply.github.com>
2021-10-31 23:14:39 -0600

    Delete todolist-assignment directory

--- todolist-assignment/src/js/utils/makeElement.js
+++
@@ -1,9 +0,0 @@
-const makeElement = function (templateString=``){
-    const docFragment = document.createRange().createContextualFragment(templateString)
-    const element = docFragment.children[0]
-    return element 
-}
-
-export default makeElement
-
- 
